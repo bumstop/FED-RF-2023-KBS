11e6891802a2dffe3abdac4c0b71945cdf83a72f
bumstop <127211764+bumstop@users.noreply.github.com>
2023-09-13 18:11:32 +0900

    cm

--- 001.HTML학습/05.Nav_PJ/js/nav06.js
+++ 001.HTML학습/05.Nav_PJ/js/nav06.js
@@ -1,14 +1,35 @@
-
-
 const domFn = {
     // 요소선택함수 ////////
     qs: (x) => document.querySelector(x),
     qsEl: (el, x) => el.querySelector(x),
     qsa: (x) => document.querySelectorAll(x),
     qsaEl: (el, x) => el.querySelectorAll(x),
-  
+
     // 이벤트셋팅함수
     addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
-  }; /////// domFn 객체 /////////////
+}; /////// domFn 객체 /////////////
 
-const gnbBox = domFn.qs('.gnb');
+const gnbBox = domFn.qs(".gnb");
+let hcode ='';
+
+for()
+`<li>
+    <a href="">1차</a>
+    <div class="smenu">
+        <aside class="smbx">
+            <h2>
+                <div class="stit">2차</div>
+                <a href="">전체보기</a>
+                <div class="swrap">
+                    <dl>
+                        <dt></dt>
+                        <dd>
+                            <a href="">요기</a>
+                        </dd>
+                    </dl>
+                </div>
+            </h2>
+        </aside>
+    </div>
+</li>`;
+// ul>li>a{1차}+.smenu>aside.smbx>h2>(.stit{2차}+a{전체보기})+.swrap>dl>dt+dd>a{요기}
