d1afb88d4096d8f58c78f6b54fff1931c271c21e
bumstop <127211764+bumstop@users.noreply.github.com>
2023-09-15 16:24:18 +0900

    cm

--- 700. 쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700. 쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -38,7 +38,7 @@ function loadFn() {
     *************************************************************/
     // 1. 대상선정: .live-box
     const liveBox = domFn.qs(".live-box");
-    console.log("대상:", liveBox);
+    // console.log("대상:", liveBox);
 
     // 2. 현장포토 데이터를 기반으로 HTML코드 만들기
     let hcode = "<ul>";
@@ -63,4 +63,52 @@ function loadFn() {
 
     // 3. 대상박스에 html 코드 넣기
     liveBox.innerHTML = hcode;
+}
+
+// [ gnb 서브메뉴 셋팅하기 ]
+
+// 1. 대상선정 : .gnb > ul > li
+// 서브메뉴 넣을 li는 하위 a요소의 텍스트가 gnbData 속성명 1차메뉴와 일치하는 경우
+// 하위 메뉴를 넣어준다.
+const gnbList = domFn.qsa('.gnb>ul>li');
+// 3. 대상에 하위메뉴 태그 만들기
+
+gnbList.forEach(ele => {
+    // 1. 하위 a요소 텍스트 읽기
+    let atxt = domFn.qsEl(ele,'a').innerText;
+    // 2. gnb데이터 읽기
+    let gData = gnbData[atxt];
+    // console.log(gnbData[atxt])
+
+    // 3. 해당 서브 데이터가 있을 경우 태그 만들어 넣기
+    if(gData){ // 데이터가 없으면 undefined -> false 처리!
+        console.log('만들어!',atxt)
+        ele.innerHTML += `
+            <div class="smenu">
+                <aside class="smbx">
+                    <h2>${atxt}</h2>
+                    <ol>
+                        ${gData.map(val => `
+                            <li>
+                                <a href="#">${val}</a>
+                            </li>
+                        `).join('')}
+                    </ol>
+                </aside>
+            </div>
+        `;
+
+        domFn.addEvt(ele, 'mouseover', overFn);
+        domFn.addEvt(ele, 'mouseout', outFn);
+
+    }
+});
+
+function overFn(){
+    let hv = domFn.qsEl(this,'.smbx').clientHeight;
+    domFn.qsEl(this,'.smenu').style.height = hv +'px';
+}
+function outFn(){
+    domFn.qsEl(this,'.smenu').style.height = '0px';
+    
 }
