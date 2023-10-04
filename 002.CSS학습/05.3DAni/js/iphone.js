33aaa9a0fe3baf58e7a5758d2cffe3a734e1430b
bumstop <127211764+bumstop@users.noreply.github.com>
2023-10-04 15:52:22 +0900

    cm

--- 002.CSS학습/05.3DAni/js/iphone.js
+++ 002.CSS학습/05.3DAni/js/iphone.js
@@ -16,18 +16,31 @@ import dFn from "./dom.js";
 // 0. 데이터 셋팅: x,y축 회전 각도를 배열에 셋팅!
 const iDeg = [
     // 상단영역
-    [25, -60],
-    [25, -30],
-    [25, 0],
-    [25, 30],
-    [25, 60],
+    [30, -60, "mv1.jpg"],
+    [30, -30, "mv2.jpg"],
+    [30, 0, "mv3.jpg"],
+    [30, 30, "mv4.jpg"],
+    [30, 60, "mv5.jpg"],
+    // 중간영역
+    [0, -60, "mv6.jpg"],
+    [0, -30, "mv7.jpg"],
+    [0, 0, "mv8.jpg"],
+    [0, 30, "mv9.jpg"],
+    [0, 60, "pt1.jpg"],
     // 하단영역
-    [-25, -60],
-    [-25, -30],
-    [-25, 0],
-    [-25, 30],
-    [-25, 60],
+    [-30, -60, "pt2.jpg"],
+    [-30, -30, "pt3.jpg"],
+    [-30, 0, "pt4.jpg"],
+    [-30, 30, "pt5.jpg"],
+    [-30, 60, "pt6.jpg"],
 ];
+
+// 이벤트 박스에 속박스 넣기
+const eBox = dFn.qs(".evt-box");
+
+for (let i = 0; i < 15; i++) {
+    eBox.innerHTML += "<div></div>";
+}
 
 // 1. 대상선정
 // 1-1. 이벤트 대상: .evt-box div
@@ -36,8 +49,8 @@ const evtBox = dFn.qsa(".evt-box div");
 const iphone = dFn.qs(".iphone");
 console.log("대상:", evtBox, iphone);
 // 1-3. 스크린: #screen
-
-
+const screen = dFn.qs("#screen");
+console.log(screen);
 // 2. 이벤트 설정
 evtBox.forEach((ele, idx) => {
     dFn.addEvt(ele, "mouseover", () => seeMe(idx));
@@ -53,4 +66,48 @@ function seeMe(seq) {
 
     // 트랜지션 변경하기
     iphone.style.transition = `.4s ease-out`;
+
+    // 2. 변경적용하기: 앞면 이미지
+    // screen.style.backgroundColor = 'white';
+    screen.style.backgroundImage = `url("./images/imgs_moving/${iDeg[seq][2]}")`;
 }
+
+// 이벤트영역 5초후  작동되도록 width값 변경하기
+setTimeout(() => {
+    eBox.style.width = "100vw";
+}, 5000);
+
+////////////////////////////////////////////
+// 마우스 포인터 변경하기! ///////////////////
+// 대상: .evt-box -> eBox변수
+dFn.addEvt(eBox, "mouseenter", function () {
+    console.log("마우스 포인터바뀜");
+    // 1. 이 박스범위안에서 커서 없애기
+    this.style.cursor = "none";
+
+    // 2. 커서박스 읽어와서 셋팅하기
+    let cursorImg = dFn.qs(".cursor-box");
+    cursorImg.style.position = "fixed";
+    cursorImg.style.width = "150px";
+    cursorImg.style.height = "250px";
+    cursorImg.style.background = "url(./images/capma.png) no-repeat 0/100% 100%";
+
+    // 3.이 박스 범위에서 mousemove이벤트 발생시 커서 위치이동셋팅
+    dFn.addEvt(this, "mousemove", (e) => {
+        cursorImg.style.top = e.pageY + "px";
+        cursorImg.style.left = e.pageX + "px";
+    });
+}); /////////////// mouseenter 함수 ////////////
+
+/* 
+    [ 마우스 오버 / 아웃 관련 이벤트 차이점 ]
+    1.mouseover / mouseout: 요소 자체를 기준 
+    2. mouseenter / mouseleave: 요소 경계선 기준
+    -> 둘의 차이는 이벤트 버블링에 있다.
+    -> 경계선 기준의 이벤트인 mouseenter / mouseleave는
+    자체 요소에서만 발생하고 버블링되지않는다.
+
+    -> 자손요소에서 버블링되어 발생하는 mouseover/mouseout으로
+    셋팅할 경우 빈번한 이벤트발생이 문제가 될 경우
+    mouseenter/mouseleave를 사용할 것을 w3c가 권고함!
+*/
