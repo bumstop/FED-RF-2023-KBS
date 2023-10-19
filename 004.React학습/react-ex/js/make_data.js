facb607cb6ffde7069436890ca6ab9fb59fab22d
bumstop <127211764+bumstop@users.noreply.github.com>
2023-10-19 17:41:21 +0900

    cm

---
+++ 004.React학습/react-ex/js/make_data.js
@@ -0,0 +1,22 @@
+// 데이터 유형에 맞게 만들기
+
+let category = ['man', 'women'];
+let gname = ['반스케주얼','반스어얼리','빈스뉴진스','반스스타일'];
+let gprice = [''];
+let aa = ["dfdfd", "aaaaa", "bbbbb", "ddddd"];
+let bb = "";
+let cc = (x) => Math.floor(Math.random() * x);
+
+
+for (let x = 1; x <= 19; x++) {
+    bb += `
+    {
+            "idx":${x2},
+            "gname":"나는야옷이야1",
+            "category":"men",
+            "gprice":"230000"
+    },
+`;
+}
+
+// console.log(bb)
