cd1bb9056a51665319f288b2c598576393b2316f
bumstop <127211764+bumstop@users.noreply.github.com>
2023-10-25 10:12:41 +0900

    cm

--- 700. 쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/common.js
+++ 700. 쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/common.js
@@ -1,17 +1,21 @@
-// 보그 PJ - 공통 모듈 JS
+// 보그 PJ - 공통 모듈 JS : common.js
 
-// DOM 메서드 불러오기
-import dFn from "./dom.js";
+// DOM 메서드
+import dFn from './dom.js';
 
-// 상단, 하단 공통 데이터 불러오기
-import tData from "./data/com_module.js";
+// 상단,하단 공통 데이터 불러오기
+import tData from './data/com_module.js';
+
+
+// [2] 상단/하단 공통 모듈 넣기 ////////
 
 // 대상선정: .common-area
 const comArea = dFn.qsa('.common-area');
 
-console.log(tData, comArea);
+// console.log(tData,comArea);
 
 // 상단영역 html 넣기
 comArea[0].innerHTML = tData.topArea;
 // 하단영역 html 넣기
 comArea[1].innerHTML = tData.footerArea;
+
