e757409c2ae0457032f7c9d9120430ff5a8504fa
bumstop <127211764+bumstop@users.noreply.github.com>
2023-10-26 15:36:09 +0900

    cm

--- 700. 쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/category.js
+++ 700. 쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/category.js
@@ -1,12 +1,6 @@
 // 보그 PJ 메인 페이지 JS - main.js
 
-import dFn from "./dom.js";
 import catData from "./data/category_data.json" assert { type: "json" };
-// 부드러운 스크롤 모듈
-import { startSS, setPos } from "./smoothScroll23.js";
-
-// [1] 부드러운 스크롤 적용 //////////
-startSS();
 
 // 카테고리 페이지 기능 구현하기
 // 요구사항: url로 전달된 키값을 읽어서 페이지의 데이터를 셋팅한다
--- 700. 쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/linksys.js
+++ 700. 쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/linksys.js
@@ -9,8 +9,8 @@ $(() => {
     // 1. 대상선정
     // 1-1. 로고링크 : .logo a
     const logo = $(".logo a");
-    // 1-2. GNB 메뉴 : .gnb a
-    const gnb = $(".gnb a");
+    // 1-2. GNB 메뉴 : .gnb a + 모바일메뉴 추가 .mognb a
+    const gnb = $(".gnb a, .mognb a");
     // console.log('대상:',logo,gnb);
 
     // 2. 이벤트 설정 및 이동기능 구현하기
@@ -37,7 +37,7 @@ $(() => {
     **************************************************/
     // $('.sns a:last').before(`<헐></헐>`);
     // $('.sns a:last').after(`<크></크>`);
-    $(".sns a:last").before(`
+    $(".sns a:last, .mosns a:last").before(`
             <a href="#" class="fi fi-laptop">
                 <span class="ir">로그인</span>
             </a>
@@ -53,7 +53,7 @@ $(() => {
     // 새로추가된 a요소까지 다시 선택하여
     // each() 메서드로 돌면서 글자를 읽어와서
     // title속성으로 넣는다! -> attr('title',값)
-    $(".sns a")
+    $(".sns a, .mosns a")
         .each((idx, ele) => {
             // ele - 각a요소
             $(ele).attr("title", $(ele).text().trim());
@@ -106,4 +106,7 @@ $(() => {
                 window.open(url);
             }
         }); /////////// click /////////////////
+
+        // 모바일 메뉴박스의 링크중 4번째 요소 다음에 br태그 삽입
+        $('.mosns a').eq(3).after('<br />'); 
 }); //////////// jQB ///////////////////
