056fa58df33ef75c79b1b4e926c63350fd6d8f60
bumstop <127211764+bumstop@users.noreply.github.com>
2023-10-31 17:07:19 +0900

    cm

--- 005.jQuery학습/003.응용구현/jq_board/js/list.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/list.js
@@ -1,22 +1,85 @@
 // 리스트 페이지 JS - list.js
 
 // 게시판 데이터 불러오기
-import bData from './data.json' assert {type:'json'};
+import bData from "./data.json" assert { type: "json" };
 
+// console.log(bData);
 
-bData.sort((a,b) => a.idx - b.idx);
-// 데이터를 화면 리스트 코드로 변환하여 적용한다.
+// 데이터 idx의 내림차순으로 정렬변경하기!
+bData.sort((a, b) => a.idx - b.idx);
+
+// 데이터를 화면 리스트 코드로 변환하여 적용한다!
 // 대상: #board tbody
-const board = $('#board tbody');
+const board = $("#board tbody");
 
-board.html(
-    bData.map(v=> `
+// 리스트 번호변수
+let listNum = 0;
+// 숫자 1씩 증가 함수
+const addNum = () => listNum++;
+
+console.log("증가수:", addNum());
+
+// 페이징되는 리스트 만들기
+// 페이징의 기본 원리 :
+// 1. 한페이지당 리스트 수를 정해 전체 페이지 개수 구하기
+// 2. 개수만큼 리스트에 데이터를 반복문으로 생성함
+// 3. 페이지 번호에 따라 시작번호를 업데이트 한다
+
+// 페이징 관련 변수들
+// [1] 한 페이지당 리스트 수 : pgBlock
+const pgBlock = 10;
+// [2] 페이지 순번 : pgNum
+let pgNum = 1;
+// [3] 전체 레코드 수 : totalCnt
+const totalCnt = bData.length;
+// [4] 페이징 블록 계산하기
+let pagingBlock = Math.ceil(totalCnt / pgBlock);
+// [5] 나머지 리스트 여부 : 0이면 다음페이지 없음!
+let addOver = totalCnt % pgBlock;
+
+// 리스트 블록으로 리스트 소스 만들기
+for (let i = 0; i <= pgBlock * pgNum; i++) {
+    board.append(`
+        <tr>
+            <td>${addNum()}</td>
+            <td>${v.tit}</td>
+            <td>${v.writer}</td>
+            <td>${v.date}</td>
+            <td>${v.cnt}</td>
+        </tr>
+    `);
+}
+
+console.log(`
+    pgBlock:${pgBlock}
+    pgNum:${pgNum}
+    totalCnt:${totalCnt}
+    pagingBlock:${pagingBlock}
+    addOver:${addOver}
+`);
+
+// 데이터 태그 생성후 태그넣기
+// board.html(
+//     bData.map((v) => `
+//         <tr>
+//             <td>${addNum()}</td>
+//             <td>${v.tit}</td>
+//             <td>${v.writer}</td>
+//             <td>${v.date}</td>
+//             <td>${v.cnt}</td>
+//         </tr>
+//     `).join("")
+// ); ///////// html //////////
+
+// 데이터를 태그로 구성함
+// 태그 구조: <tr><td></td>...</tr>
+/* 
+    [ 매칭 데이터 ]
     <tr>
-        <th>${v.idx}</th>
-        <th>${v.tit}</th>
-        <th>${v.writer}</th>
-        <th>${v.date}</th>
-        <th>${v.cnt}</th>
+        <td>번호 - idx</td>
+        <td>글 제목 - tit</td>
+        <td>글 쓴이 - writer</td>
+        <td>등록일자 - date</td>
+        <td>조회수 - cnt</td>
     </tr>
-    `).join('')
-);
+*/
