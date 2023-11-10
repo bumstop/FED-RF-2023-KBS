b662803c2c273ad99592a82a65fd8540d3ec274d
bumstop <127211764+bumstop@users.noreply.github.com>
2023-11-10 17:54:29 +0900

    cm

--- 004.React학습/react-app/kbs-app/my-fisrt-react-app/src/dc/contents/Main.jsx
+++ 004.React학습/react-app/kbs-app/my-fisrt-react-app/src/dc/contents/Main.jsx
@@ -1,12 +1,12 @@
 import { Banner } from '../contents/Banner';
 
-export function Main(props) {
+export function Main() {
     return (
         <>
             <h1 style={{textAlign:"center"}}>
                 메인페이지
             </h1>
-            <Banner category={props.cat} />
+            <Banner category="main" />
         </>
     )
 }
