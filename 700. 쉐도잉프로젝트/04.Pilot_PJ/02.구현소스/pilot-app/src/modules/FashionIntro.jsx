ef864353a55525a55448d9464aced9f5f08baa5a
bumstop <127211764+bumstop@users.noreply.github.com>
2023-11-17 15:33:55 +0900

    cm

---
+++ 700. 쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/FashionIntro.jsx
@@ -0,0 +1,28 @@
+// Pilot PJ - 패션인트로 컴포넌트 ////////
+// 패션 인트로 데이터 불러오기
+import { fsData } from "../data/fashion_intro";
+
+// 패션 인트로 CSS 불러오기
+import "../css/fashion_intro.css";
+
+export function FashionIntro(props) {
+  // props.cat - 카테고리 분류명
+
+  return (
+    <div id={props.cat} className="fs-page">
+      <ul className="pgc">
+        <li className="imgc">
+          <img src={selData.isrc} alt={selData.ialt} />
+        </li>
+        <li className="txtc">
+          <h2>
+            <a href="#">
+              {selData.tit.split('^')}
+              {selData.tit.split('^')}
+            </a>
+          </h2>
+        </li>
+      </ul>
+    </div>
+  );
+} //////////// FashionIntro 컴포넌트 ////////
