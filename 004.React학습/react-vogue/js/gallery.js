e99f73e2fd8a71dce672aba8d8506cc60e482bc4
bumstop <127211764+bumstop@users.noreply.github.com>
2023-11-17 11:18:30 +0900

    cm

--- 004.React학습/react-vogue/js/gallery.js
+++ 004.React학습/react-vogue/js/gallery.js
@@ -1,41 +1,68 @@
 // 보그 PJ 갤러리 페이지 JS - gallery.js
 
 var swiper = new Swiper(".mySwiper", {
-    autoplay: {
-        // 자동넘김 시간
-        delay: 2500,
-        // 상호작용(건들여놓기)에 대한 재가동 없애기 안씀(false)
-        disableOnInteraction: false,
+  autoplay: {
+    // 자동넘김 시간
+    delay: 1000,
+    // 상호작용(건들여놓기)에 대한 재가동 없애기 안씀(false)
+    disableOnInteraction: false,
+  },
+  // 한번에 보일 슬라이드 수
+  slidesPerView: 4,
+  // 사이간격
+  spaceBetween: 5,
+  // 하단 블릿
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 200px
+    200: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    // 한번에 보일 슬라이드 수
-    slidesPerView: 4,
-    // 사이간격
-    spaceBetween: 5,
-    // 하단 블릿
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+    // when window width is >= 700px
+    700: {
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    // when window width is >= 1000px
+    1000: {
+      slidesPerView: 4,
+      spaceBetween: 40,
     },
-    breakpoints: {
-        // when window width is >= 200px
-        200: {
-          slidesPerView: 2,
-          spaceBetween: 20
-        },
-        // when window width is >= 700px
-        700: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        // when window width is >= 1000px
-        1000: {
-          slidesPerView: 4,
-          spaceBetween: 40
-        }
-      }
+  },
 });
+// 스와이퍼의 기능을 외부에서 사용하기 위해 스와이퍼 생성시 변수에 할당한것임
+// 공식 사이트의 API를 참조하여 여러가지 설정 및 메서드를 사용하여 기능을 외부에 부여할 수 있다
+// 예) 상단 타이틀을 클릭하면 다음 슬라이드로 이동
+
+$(".stopPlay")
+  .css({
+    backgroundColor: "#fff",
+    border: "none",
+    fontSize: "40px",
+    display: "block",
+    width: "40px",
+    margin: "0 auto",
+    cursor: "pointer",
+  })
+  .attr("title", "멈추기")
+  .click((e) => {
+    let icon = $(e.target).text();
+    console.log(icon);
+    if (icon == "▣") {
+      swiper.autoplay.pause();
+      $(e.target).text("▶").attr("title", "자동넘기기");
+    } else {
+      swiper.autoplay.start();
+      $(e.target).text("▣").attr("title", "멈추기");
+    }
+  });
+// $('.stit').click(() => swiper.slideNext());
--- 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperApp.jsx
+++ 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperApp.jsx
@@ -1,10 +1,11 @@
 // 스와이퍼 플러그인 컴포넌트
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import $ from "jquery";
+import "jquery-ui-dist/jquery-ui";
 // Import Swiper styles
-
+import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./css/swiper.css";
@@ -14,9 +15,42 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 export function SwiperApp() {
   const imgArr = ["dcm18", "dcm19", "dcm20", "dcm21", "dcm22", "dcm23", "dcm24", "dcm25"];
+
+  // 상태관리변수: 멈춤상태 / 플레이상태
+  const [sts, setSts] = useState(1);
+
+  // Swiper 컴포넌트의 ref속성에 담아서 연결!
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    $(".stopPlay")
+      .css({
+        backgroundColor: "transparent",
+        border: "none",
+        fontSize: "40px",
+        display: "block",
+        width: "40px",
+        margin: "0 auto",
+        cursor: "pointer",
+      })
+      .attr("title", "멈추기");
+  }, []);
+
+  // 플레이/멈춤 기능 함수
+  const stopPlay = () => {
+    console.log("0. ", sts);
+
+    sts ? setSts(0) : setSts(1);
+    console.log("1. ", sts);
+    
+    sts ? myRef.current.swiper.autoplay.stop() : myRef.current.swiper.autoplay.start();
+    console.log("2. ", sts);
+  }; // const stopPlay
+
   return (
     <>
       <Swiper
+        ref={myRef}
         slidesPerView={3}
         spaceBetween={30}
         pagination={{
@@ -33,6 +67,9 @@ export function SwiperApp() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <button className="stopPlay" onClick={stopPlay}>
+        {sts ? "▣" : "▶"}
+      </button>
     </>
   );
 }
