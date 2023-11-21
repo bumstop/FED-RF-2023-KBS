66e1aeb56fba165606b807a8dee78a897065fbb0
bumstop <127211764+bumstop@users.noreply.github.com>
2023-11-21 12:02:56 +0900

    cm

--- 700. 쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700. 쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -1,45 +1,52 @@
 // 메인 페이지 JS - index.js
 import React, { useEffect, useState } from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
-import $ from "jquery";
-// import layout component
 import { TopArea } from "./layout/TopArea";
 import { MainArea } from "./layout/MainArea";
 import { FooterArea } from "./layout/FooterArea";
 
-// Import common CSS
+// 제이쿼리
+import $ from "jquery";
+import "jquery-ui-dist/jquery-ui";
+
+// 페이지 공통 CSS
 import "./css/common.css";
 
-// 최상위 Root 컴포넌트
+// 최상위 Root 컴포넌트 ///////
 function App() {
-  // 후크상태변수 설정: 페이지변경
+  // 후크상태변수 설정 : 페이지변경
   const [pgName, setPgName] = useState("main");
 
   // 페이지변경 상태변수 업데이트 함수
   const chgPgName = (txt) => {
     setPgName(txt);
-  }; // const chgPgName
+  }; ///////// chgPgName 함수 //////
 
-  // 랜더링후 실행구역
+  // 랜더링 후 실행구역 ////////////
   useEffect(() => {
-    $(".gnb li, .indic li").on("click", function () {
-      let idx = $(this).index();
-      console.log("나야나", idx);
+    // 햄버거 버튼 클릭시 전체 메뉴 보이기/숨기기
+    $(".ham").click((e) => {
+      // 1. 전체메뉴 박스 : .mbox -> 보이기/숨기기
+      $(".mbox").fadeToggle(400);
 
-      // 페이지이동
-      $("html, body").animate(
-        {
-          scrollTop: $(window).height() * idx + "px",
-        },
-        800,
-        "easeInOutQuint"
-      ); // animate
-      // 클릭된 메뉴에 class 'on' 넣기
-      $('.gnb li').eq(idx).addClass("on").siblings().removeClass("on");
-      $('.indic li').eq(idx).addClass("on").siblings().removeClass("on");
-    });
-  });
+      // 2. 햄버거버튼에 클래스 'on' 넣기/빼기
+      $(e.currentTarget).toggleClass("on");
+      // e.target과 e.currentTarget은 다르다!
+      // 후자가 햄버거 버튼 자신임!
+      // console.log(e.currentTarget)
 
+      // 3. 비디오 재생/멈춤 : 대상 - .bgm
+      // get(0)은 비디오컬렉션임! -> 제이쿼리용
+      const vid = $(".bgm").get(0);
+      vid.paused ? vid.play() : vid.pause();
+      // console.log(vid.paused);
+      // paused 속성 : 동영상 멈춤일때 true 리턴
+      // play() 메서드 : 동영상 재생 메서드
+      // pause() 메서드 : 동영상 정지 메서드
+    }); //////// click ////////
+  }); ////////// useEffect //////////////
+
+  // 리턴코드 //////////////////////////
   return (
     <>
       <TopArea cat={pgName} />
@@ -47,13 +54,23 @@ function App() {
       <FooterArea />
     </>
   );
-} // function App()
+} ///////////// App 컴포넌트 /////////////
 
 /* 
-  <button onClick={() => chgPgName("main")}>메인 페이지</button>
-  <button onClick={() => chgPgName("men")}>남성 페이지</button>
-  <button onClick={() => chgPgName("women")}>여성 페이지</button>
-  <button onClick={() => chgPgName("style")}>스타일 페이지</button> 
+<button onClick={()=>chgPgName('main')}>
+  메인페이지
+</button>
+<button onClick={()=>chgPgName('men')}>
+  남성페이지
+</button>
+<button onClick={()=>chgPgName('women')}>
+  여성페이지
+</button>
+<button onClick={()=>chgPgName('style')}>
+  스타일페이지
+</button>
 */
+
+// 출력하기 ///////
 const root = createRoot(document.querySelector("#root"));
 root.render(<App />);
