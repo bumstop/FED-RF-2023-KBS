5f5d2c32bc8c81f41da50b03b20d62760155067a
bumstop <127211764+bumstop@users.noreply.github.com>
2023-11-21 15:40:23 +0900

    cm

--- 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidSwipe.jsx
+++ 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidSwipe.jsx
@@ -15,17 +15,21 @@ import "../../css/vid_swipe.css";
 import { SwiperVid } from "../plugin/SwiperVid";
 
 export function VidSwipe(props) {
-  // props.tit - 비디오 스와이프 타이틀
+  // props.cat - 카테고리명
 
+  const catTit = {
+    main:"LATEST TRAILERS, CLIPS & MORE",
+    moveis: "TRAILERS, CLIPS AND MORE",
+  };
   // 리턴코드 //////////////////
   return (
     <>
       {/* 모듈코드 */}
       <section className="vid-swbox">
         {/* 1. 모듈타이틀 */}
-        <h2 className="tit">{props.tit}</h2>
+        <h2 className="tit">{catTit[props.cat]}</h2>
         {/* 2. 스와이퍼 컴포넌트 : SwiperVid */}
-        <SwiperVid />
+        <SwiperVid cat={props.cat}/>
 
         {/* 3. 비디오 재생창 */}
         <section className="vid-bx">
