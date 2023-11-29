e5a53552c9402bff181924bd6c81b8b1ce36f5fa
bumstop <127211764+bumstop@users.noreply.github.com>
2023-11-29 13:06:07 +0900

    Update Searching.jsx

--- 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Searching.jsx
+++ 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Searching.jsx
@@ -143,45 +143,65 @@ export function Searching(props) {
     console.log("아이디:", cid, chked);
 
     // 3. 기존 입력 데이터 가져오기
+    // selData의 첫번째 배열값
     let temp = selData[0];
 
     // 결과집합배열변수 : 최종결과배열
     let lastList = [];
-    // 4. 체크박스 체크개수 세기 : 1개 초과시 배열합치기
+
+    // 4.체크박스 체크개수세기 : 1개초과시 배열합치기!
     let num = $(".chkhdn:checked").length;
-    const nowList = catListData.filter((v) => {
-      if (v.alignment === cid) return true;
-    });
-    // 5. 체크박스 체크 유무에따른 분기
-    // (1) 체크박스가 true 일때 해당 검색어로 검색
+    console.log("체크개수:", num);
+
+    // 5. 체크박스 체크유무에 따른 분기
+    // (1) 체크박스가 true일대 해당 검색어로 검색하기
     if (chked) {
-      // 현재데이터 변수에 담기
-     
-      // 1개 초과시 배열합치기
+      // 현재데이터 변수에 담기(수정예정!)
+      const nowList = catListData.filter((v) => {
+        if (v.alignment == cid) return true;
+      }); /////////// filter //////////
+
+      // 체크개수가 1초과일때 배열합치기
       if (num > 1) {
+        // 스프레드 연산자(...)사용!
         lastList = [...temp, ...nowList];
-        return;
-      }
-      if (num <= 1) {
+      } //// if /////
+      else {
+        // 하나일때
         lastList = nowList;
-        return;
       }
-      console.log(lastList);
-    } else {
-      console.log("지울데이터", cid);
-      // for문을 돌면서 지울 데이터를 찾는다
-      for (let i = 0; i < temp; i++) {
+    } /////////// if /////////
+    // (2) 체크박스가 false일때 데이터 지우기
+    else {
+      console.log("지울데이터:", cid);
+      // for문을 돌면서 배열데이터중 해당값을 지운다!
+      for (let i = 0; i < temp.length; i++) {
+        // -> 삭제대상:
         // 데이터중 alignment 항목값이 아이디명과 같은것
         if (temp[i].alignment == cid) {
-          // temp.splice(i, 1);
-          // i--;
-          delete temp[i];
-        }
-      }
-      lastList = temp;
-    }
+          // 해당항목 지우기
+          // 배열지우기 메서드 : splice(순번,개수)
+          temp.splice(i, 1);
+          // 주의! 배열을 지우면 전체개수가 1씩줄어든다!
+          // 반드시 줄임처리할것!
+          i--;
 
-    // 5. 검색결과 리스트 업데이트하기
+          // 참고테스트 : 배열삭제 delete는 무엇인가?
+          // delete배열지우기는 값만지우고 주소는 남는다!
+          // 지운후 값은 undefined로 남아진다!
+          // delete temp[i];
+          // -> 리스트처리시 에러발생함!
+          // 여기서는 splice를 반드시 사용할것!
+        } //////// if ///////
+      } ///////// for ////////
+
+      console.log("삭제처리된배열:", temp);
+
+      // 결과처리하기 : 삭제처리된 temp를 결과에 넣기!
+      lastList = temp;
+    } /////////// else ///////////
+
+    // 6. 검색결과 리스트 업데이트 하기
     setSelData([lastList, 2]);
     setCnt(lastList.length);
   }; ////////////// chkSearch 함수 ///////////
