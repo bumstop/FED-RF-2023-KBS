9a8418a162e717b7b662d4726ad93d3094f79bf5
bumstop <127211764+bumstop@users.noreply.github.com>
2023-12-05 12:01:54 +0900

    cm

--- 700. 쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/ItemDetail.jsx
+++ 700. 쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/ItemDetail.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { sinsangData } from "../data/sinsang";
 
 import $ from "jquery";
+import { CartList } from "./CartList";
 
 export function ItemDetail({ cat, goods }) {
   // cat - 카테고리명(men/women/style)
@@ -22,26 +23,45 @@ export function ItemDetail({ cat, goods }) {
     $(".bgbx").slideUp(400);
   };
 
-  // 렌더링 후 실행구역
+  // 랜더링후 실행구역 ///////
   useEffect(() => {
+    // 숫자출력 input
     const sum = $("#sum");
+    // 수량증감 이미지버튼
     const numBtn = $(".chg_num img");
-    
+
+    // 수량 증감 함수 /////////
     numBtn.click((e) => {
+      // 이미지순번
       let seq = $(e.currentTarget).index();
+      // 기존값 읽기
       let num = Number(sum.val());
+      // 윗버튼은 ++, 아랫버튼은 --
       seq ? num-- : num++;
+      // 한계값
       if (num < 1) num = 1;
       console.log("순번:", seq, num);
+      // 증감 반영
       sum.val(num);
-      $('#total').text((selData[2] * num).toLocaleString() + "원")
-     
+      // 총합계 반영
+      // 기본값 : selData[2]
+      // 출력박스 : #total
+      $("#total").text(addComma(selData[2] * num) + "원");
     });
-  }, []);
-  // 리랜더링 실행구역
+  }, []); ////  한번만 실행 /////
+
+  // 리랜더링 실행구역 /////
   useEffect(() => {
-    $("#sum").val(1);
-  })
+    // 수량초기화
+    $("#sum").val("1");
+    // 총합계초기화
+    $("#total").text(addComma(selData[2]) + "원");
+  }); ////////// useEffect //////
+
+  //정규식함수(숫자 세자리마다 콤마해주는 기능)
+  function addComma(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
   // 리턴코드 ///////////////////////////
   return (
@@ -65,7 +85,7 @@ export function ItemDetail({ cat, goods }) {
             </div>
           </section>
           <section className="gdesc scbar">
-            <h1>HOME &gt; {cat.toUpperCase()}</h1>
+            <h1>HOME &gt; MEN</h1>
             <div>
               <ol>
                 <li>
@@ -80,7 +100,7 @@ export function ItemDetail({ cat, goods }) {
                 </li>
                 <li>
                   <span>판매가</span>
-                  <span id="gprice">{Number(selData[2]).toLocaleString() + "원"}</span>
+                  <span id="gprice">{addComma(selData[2])}원</span>
                 </li>
                 <li>
                   <span>적립금</span>
@@ -122,7 +142,8 @@ export function ItemDetail({ cat, goods }) {
                   <span>권장계절</span> <span>여름</span>
                 </li>
                 <li className="tot">
-                  <span>총합계</span> <span id="total">{Number(selData[2]).toLocaleString() + "원"}</span>
+                  <span>총합계</span>
+                  <span id="total">{addComma(selData[2])}원</span>
                 </li>
               </ol>
             </div>
@@ -134,6 +155,9 @@ export function ItemDetail({ cat, goods }) {
           </section>
         </div>
       </div>
+
+      {/* 카트리스트 */}
+      <CartList />
     </>
   );
 } /////////// ItemDetail 컴포넌트 ///////////
