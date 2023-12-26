6285c8ea69c568f62dfca57e1715dc818159fa38
bumstop <bums1290@gmail.com>
2023-12-26 12:11:25 +0900

    cm

---
+++ 003.JS학습/JS4-6.배열의복사검증.js
@@ -0,0 +1,82 @@
+// JS4-6.배열의 복사
+const arrData = [
+    
+    {
+        "idx" : "1",
+        "tit" : "This is a Title1",
+        "cont" : "I wanna talk to you now1",
+        "att" : "",
+        "date" : "2023-06-01",
+        "writer" : "admin",
+        "pwd" : "1111",
+        "cnt" : "1"
+    },
+
+    {
+        "idx" : "2",
+        "tit" : "This is a Title2",
+        "cont" : "I wanna talk to you now2",
+        "att" : "",
+        "date" : "2023-06-01",
+        "writer" : "admin",
+        "pwd" : "1111",
+        "cnt" : "1"
+    },
+
+    {
+        "idx" : "3",
+        "tit" : "This is a Title3",
+        "cont" : "I wanna talk to you now3",
+        "att" : "",
+        "date" : "2023-06-01",
+        "writer" : "admin",
+        "pwd" : "1111",
+        "cnt" : "1"
+    },
+
+    {
+        "idx" : "4",
+        "tit" : "This is a Title4",
+        "cont" : "I wanna talk to you now4",
+        "att" : "",
+        "date" : "2023-06-01",
+        "writer" : "admin",
+        "pwd" : "1111",
+        "cnt" : "1"
+    },
+
+    {
+        "idx" : "5",
+        "tit" : "This is a Title5",
+        "cont" : "I wanna talk to you now5",
+        "att" : "",
+        "date" : "2023-06-01",
+        "writer" : "admin",
+        "pwd" : "1111",
+        "cnt" : "1"
+    },
+]
+
+// 얕은복사
+const copyData = arrData;
+
+// 깊은복사
+// const copyData = JSON.parse(JSON.stringify(arrData));
+
+
+// 데이터변경
+copyData.some((v) => {
+    if(Number(v.idx) === 3) {
+        v.tit = '나야나야나! ^^';
+        return true;
+    }
+});
+
+// 내림차순변경
+copyData.sort((x, y) => {
+    return Number(x.idx) ==Number( y.idx) ? 0 : Number(x.idx) > Number(y.idx) ? -1 : 1;
+});
+
+// 원본과 카피본 확인하기
+console.log('원본', arrData);
+console.log('카피본', copyData);
